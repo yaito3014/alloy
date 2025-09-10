6f8ff2fa69b61aa8b78ff3223bc9d6e7283c099f
yaito3014 <ykakeyama3014@gmail.com>
2025-09-10 12:31:47 +0900

    Require `adaptor<T>::getters` to be specialization of `non_type_list`

--- include/yk/alloy/adaptor.hpp
+++ include/yk/alloy/adaptor.hpp
@@ -1,13 +1,19 @@
 #ifndef YK_ALLOY_ADAPTOR_HPP
 #define YK_ALLOY_ADAPTOR_HPP
 
+#include "yk/alloy/detail/specialization_of.hpp"
+#include "yk/alloy/non_type_list.hpp"
+
 namespace yk::alloy {
 
 template<class T>
 struct adaptor;
 
 template<class T>
-concept adapted = requires { typename adaptor<T>::getters; };
+concept adapted = requires {
+  typename adaptor<T>::getters;
+  requires detail::is_nttp_specialization_of_v<typename adaptor<T>::getters, non_type_list>;
+};
 
 }  // namespace yk::alloy
 
