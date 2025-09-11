1ce5a80d1c7fe8bb4b7d7d5be4f5ef1262cd11d8
yaito3014 <ykakeyama3014@gmail.com>
2025-09-11 20:24:19 +0900

    Replace `forward_like_t`'s definition to use `std::forward_like`

--- include/yk/alloy/detail/forward_like_t.hpp
+++ include/yk/alloy/detail/forward_like_t.hpp
@@ -1,28 +1,12 @@
 #ifndef YK_ALLOY_DETAIL_FORWARD_LIKE_T_HPP
 #define YK_ALLOY_DETAIL_FORWARD_LIKE_T_HPP
 
-#include <type_traits>
+#include <utility>
 
 namespace yk::alloy::detail {
 
 template<class From, class To>
-struct override_ref {
-  using type = std::conditional_t<std::is_rvalue_reference_v<From>, To&&, To&>;
-};
-
-template<class From, class To>
-using override_ref_t = typename override_ref<From, To>::type;
-
-template<class From, class To>
-struct copy_const {
-  using type = std::conditional_t<std::is_const_v<From>, To const, To>;
-};
-
-template<class From, class To>
-using copy_const_t = typename copy_const<From, To>::type;
-
-template<class From, class To>
-using forward_like_t = override_ref_t<From&&, copy_const_t<std::remove_reference_t<From>, std::remove_reference_t<To>>>;
+using forward_like_t = decltype(std::forward_like<From>(std::declval<To>()));
 
 }  // namespace yk::alloy::detail
 
