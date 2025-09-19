2ca5b5d5a66990598b96aae554cc4ab867a386eb
yaito3014 <ykakeyama3014@gmail.com>
2025-09-19 18:45:57 +0900

    Write down forward_like_t for Clang

--- include/yk/alloy/detail/forward_like_t.hpp
+++ include/yk/alloy/detail/forward_like_t.hpp
@@ -1,12 +1,24 @@
 #ifndef YK_ALLOY_DETAIL_FORWARD_LIKE_T_HPP
 #define YK_ALLOY_DETAIL_FORWARD_LIKE_T_HPP
 
-#include <utility>
+#include <type_traits>
 
 namespace yk::alloy::detail {
 
 template<class From, class To>
-using forward_like_t = decltype(std::forward_like<From>(std::declval<To>()));
+using forward_like_t = std::conditional_t<
+  std::is_rvalue_reference_v<From&&>,
+  std::conditional_t<
+    std::is_const_v<std::remove_reference_t<From>>,
+    std::remove_cvref_t<To> const,
+    std::remove_cvref_t<To>
+  >&&,
+  std::conditional_t<
+    std::is_const_v<std::remove_reference_t<From>>,
+    std::remove_cvref_t<To> const,
+    std::remove_cvref_t<To>
+  >&
+>;
 
 }  // namespace yk::alloy::detail
 
